3608b278cbcaf2e3f8a0dbd33c617fe1daf26c57
Jeremy Beasley <jeremy.beasley@gmail.com>
2019-04-22 12:27:53 -0700

    Parse aguments with get_input_args()

--- intropylab-classifying-images/get_input_args.py
+++ intropylab-classifying-images/get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
+# PROGRAMMER: Jeremy Beasley 
+# DATE CREATED: 20190422                             
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
@@ -40,4 +40,15 @@ def get_input_args():
     """
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
-    return None
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dir", type=str, default="pet_images/", 
+            help="Path to the folder of pet images")
+
+    parser.add_argument("--arch", type=str, default="vgg",
+            help="CNN Model Architecture")
+
+    parser.add_argument("--dogfile", type=str, default="dognames.txt",
+            help="Text files with dog names")
+
+    return parser.parse_args()
