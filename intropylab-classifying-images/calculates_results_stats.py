d4bea42c9c4f88e52ffa78f08c76b2c9b091e188
Jeremy Beasley <jeremy.beasley@gmail.com>
2019-04-28 16:34:06 -0700

    fix: Handles division by 0

--- intropylab-classifying-images/calculates_results_stats.py
+++ intropylab-classifying-images/calculates_results_stats.py
@@ -92,9 +92,9 @@ def calculates_results_stats(results_dic):
         if val[2]==1: results_stats_dic["n_match"]+=1
 
     # Calculate percentages 
-    results_stats_dic["pct_correct_dogs"] = results_stats_dic["n_correct_dogs"]/results_stats_dic["n_dogs_img"]*100
+    results_stats_dic["pct_correct_dogs"] = results_stats_dic["n_correct_dogs"]/results_stats_dic["n_dogs_img"]*100 if results_stats_dic["n_dogs_img"] != 0 else 0
     results_stats_dic["pct_correct_notdogs"] = results_stats_dic["n_correct_notdogs"]/results_stats_dic["n_notdogs_img"]*100 if results_stats_dic["n_notdogs_img"] != 0 else 0
-    results_stats_dic["pct_correct_breed"] = results_stats_dic["n_correct_breed"]/results_stats_dic["n_dogs_img"]*100
-    results_stats_dic["pct_match"] = results_stats_dic["n_match"]/results_stats_dic["n_images"]*100
+    results_stats_dic["pct_correct_breed"] = results_stats_dic["n_correct_breed"]/results_stats_dic["n_dogs_img"]*100 if results_stats_dic["n_dogs_img"] != 0 else 0
+    results_stats_dic["pct_match"] = results_stats_dic["n_match"]/results_stats_dic["n_images"]*100 if results_stats_dic["n_images"] != 0 else 0
 
     return results_stats_dic
